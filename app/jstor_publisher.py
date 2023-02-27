996ff183b69bba7784fdc9405694f9f9007bd5ea
Chip Goines <chip_goines@harvard.edu>
2023-02-27 01:20:02 -0500

    first pass

--- app/jstor_publisher.py
+++ app/jstor_publisher.py
@@ -41,6 +41,8 @@ class JstorPublisher():
         self.aspace_s3_resource = None
         self.aspace_s3_bucket = None
         self.connect_to_buckets()
+        self.repositories = self.load_repositories()
+
         
     def connect_to_buckets(self):
         """
@@ -96,6 +98,16 @@ Update job timestamp file"""
         current_app.logger.info("Sleep " + str(sleep_s) + "seconds")
         sleep(sleep_s)
 
+        #dump json
+        current_app.logger.info("json message: " + json.dumps(request_json))
+
+        job_ticket_id = str(request_json['job_ticket_id'])
+
+        #integration test: write small record to mongo to prove connectivity
+        integration_test = False
+        if ('integration_test' in request_json):
+            integration_test = request_json['integration_test']
+
         harvestset = None
         if 'harvestset' in request_json:
             harvestset = request_json["harvestset"]
@@ -105,17 +117,27 @@ Update job timestamp file"""
             current_app.logger.info("running jstorforum publisher")
             jstorforum = request_json['jstorforum']
         if jstorforum:
-            self.do_publish('jstorforum', harvestset)
-        
+            try:
+                if (integration_test):
+                    self.do_publish('jstorforum', None, True)
+                else:
+                    self.do_publish('jstorforum', harvestset)
+                except Exception as err:
+                    current_app.logger.error("Error: unable to publish jstorforum records, {}", err)
+
         aspace = False
         if 'aspace' in request_json:
             current_app.logger.info("running aspace transform")
             aspace = request_json['aspace']
         if aspace:
-            self.do_publish('aspace', None)
+            try:
+                if (integration_test):
+                    self.do_publish('aspace', None, True)
+                else:
+                    self.do_publish('aspace', None)
+            except Exception as err:
+                current_app.logger.error("Error: unable to publish aspace records, {}", err)
 
-        #dump json
-        current_app.logger.info("json message: " + json.dumps(request_json))
 
         #integration test: write small record to mongo to prove connectivity
         integration_test = False
@@ -123,11 +145,6 @@ Update job timestamp file"""
             integration_test = request_json['integration_test']
         if (integration_test):
             current_app.logger.info("running integration test")
-            try:
-                self.do_publish('jstorforum', None, True)
-                self.do_publish('aspace', None, True)
-            except Exception as err:
-                current_app.logger.error("Error: unable to publish records, {}", err)
             try:
                 mongo_url = os.environ.get('MONGO_URL')
                 mongo_dbname = os.environ.get('MONGO_DBNAME')
@@ -159,6 +176,20 @@ Update job timestamp file"""
         transformDir = os.getenv("JSTOR_TRANSFORM_DIR")
         aspaceDir = os.getenv("JSTOR_ASPACE_DIR")
         directories = [harvestDir, transformDir]
+        mongo_url = os.environ.get('MONGO_URL')
+        mongo_dbname = os.environ.get('MONGO_DBNAME')
+        harvest_collection_name = os.environ.get('HARVEST_COLLECTION', 'jstor_harvests')
+        repository_collection_name = os.environ.get('REPOSITORY_COLLECTION', 'jstor_repositories')
+        record_collection_name = os.environ.get('RECORD_COLLECTION', 'jstor_records')
+        mongo_url = os.environ.get('MONGO_URL')
+        mongo_client = None
+        mongo_db = None
+        try:
+            mongo_client = MongoClient(mongo_url, maxPoolSize=1)
+            mongo_db = mongo_client[mongo_dbname]
+        except Exception as err:
+            current_app.logger.error("Error: unable to connect to mongodb, {}", err)
+
         #publish to VIA and SSIO
         current_app.logger.info("publishing to S3")
         for baseDir in directories:
@@ -166,8 +197,11 @@ Update job timestamp file"""
                 if jobname == 'jstorforum' and jobname == job["jobName"]:     
                     for set in job["harvests"]["sets"]:
                         setSpec = "{}".format(set["setSpec"])
+                        repository_name = self.repositories[setSpec]
                         opDir = set["opDir"]
                         currentPath = baseDir + "/" + opDir
+                        totalPublishCount = 0
+                        harvestdate = datetime.today().strftime('%Y-%m-%d') 
                         if harvestset is None:
                             current_app.logger.info("looking in current path: " + currentPath)
                             if os.path.exists(currentPath):
@@ -183,6 +217,17 @@ Update job timestamp file"""
                                             elif (baseDir == transformDir):  #send to VIA bucket
                                                 current_app.logger.info("Uploading: " + filepath + " to " + s3prefix + filename + " in the VIA bucket") 
                                                 self.via_s3_bucket.upload_file(filepath, s3prefix + filename)
+                                            totalPublishCount = totalPublishCount + 1
+                                            #write/update record
+                                            try:
+                                                status = "update"
+                                                identifier = filename[:4]
+                                                self.write_record(job_ticket_id, identifier, harvestdate, setSpec, repository_name, 
+                                                    status, record_collection_name, mongo_db)
+                                                totalPublishCount = totalPublishCount + 1    
+                                            except Exception as e:
+                                                current_app.logger.error(e)
+                                                current_app.logger.error("Mongo error writing " + setSpec + " record: " +  identifier)
                                         except Exception as err:
                                             current_app.logger.error("VIA/SSIO Publishing error: {}", err)
                         elif  setSpec == harvestset: 
@@ -201,11 +246,31 @@ Update job timestamp file"""
                                             elif (baseDir == transformDir):  #send to VIA bucket
                                                 current_app.logger.info("Uploading: " + filepath + " to " + s3prefix + filename + " in the VIA bucket") 
                                                 self.via_s3_bucket.upload_file(filepath, s3prefix + filename)
+                                            totalPublishCount = totalPublishCount + 1
+                                            #write/update record
+                                            try:
+                                                status = "update"
+                                                identifier = filename[:4]
+                                                self.write_record(job_ticket_id, identifier, harvestdate, setSpec, repository_name, 
+                                                    status, record_collection_name, mongo_db)
+                                                totalPublishCount = totalPublishCount + 1    
+                                            except Exception as e:
+                                                current_app.logger.error(e)
+                                                current_app.logger.error("Mongo error writing " + setSpec + " record: " +  identifier)
                                         except Exception as err:
-                                            current_app.logger.error("VIA/SSIO Publishing error: {}", err)        
+                                            current_app.logger.error("VIA/SSIO Publishing error: {}", err)     
+                        #update harvest record
+                        try:
+                            self.update_harvest(job_ticket_id, harvestdate, setSpec, 
+                                repository_name, totalPublishCount, harvest_collection_name, mongo_db)
+                        except Exception as e:
+                            current_app.logger.error(e)
+                            current_app.logger.error("Mongo error writing harvest record for : " +  setSpec)   
 
                 #publish to Aspace
                 if jobname == 'aspace' and jobname == job["jobName"]:                       
+                    harvestdate = datetime.today().strftime('%Y-%m-%d')
+                    totalPublishCount = 0 
                     if os.path.exists(aspaceDir):
                         if len(fnmatch.filter(os.listdir(aspaceDir), '*.xml')) > 0:
                             current_app.logger.info("Publishing to Aspace S3")
@@ -214,8 +279,71 @@ Update job timestamp file"""
                                     filepath = aspaceDir + "/" + filename
                                     current_app.logger.info("Uploading: " + filepath + " to " + filename + " in the ASPACE bucket")
                                     self.aspace_s3_bucket.upload_file(filepath, filename)
+                                    totalPublishCount = totalPublishCount + 1
+                                    #write/update record
+                                    try:
+                                        status = "update"
+                                        identifier = filename[:4]
+                                        self.write_record(job_ticket_id, identifier, harvestdate, "0000", "aspace", 
+                                            status, record_collection_name, mongo_db)
+                                        totalPublishCount = totalPublishCount + 1    
+                                    except Exception as e:
+                                                current_app.logger.error(e)
+                                                current_app.logger.error("Mongo error writing aspace record: " +  identifier)
                                 except Exception as err:
                                     current_app.logger.error("Aspace Publishing error: {}", err)
+
+                    #update harvest record
+                    try:
+                        self.update_harvest(job_ticket_id, harvestdate, "0000", 
+                            "aspace", totalHarvestCount, harvest_collection_name, mongo_db)
+                    except Exception as e:
+                        current_app.logger.error(e)
+                        current_app.logger.error("Mongo error writing harvest record for: aspace")
+    
+    def update_harvest(self, harvest_id, harvest_date, repository_id, repository_name,
+            total_published, collection_name, mongo_db):
+        if mongo_db == None:
+            current_app.logger.info("Error: mongo db not instantiated")
+            return
+        try:
+            if harvest_date == None: #set harvest date to today if harvest date is None
+                harvest_date = datetime.today().strftime('%Y-%m-%d') 
+            harvest_date_obj = datetime.strptime(harvest_date, "%Y-%m-%d")
+            query = { "id" : harvest_id }
+            harvest_record = {"$set": { "id": harvest_id, "harvest_date": harvest_date_obj, 
+                "repository_id": repository_id, "repository_name": repository_name, 
+                "total_published_count": total_published, "success": True } }
+            harvest_collection = mongo_db[collection_name]
+            #harvest_collection.insert_one(harvest_record)
+            harvest_collection.update_one(query, harvest_record, upsert=True)
+            current_app.logger.info(repository_name + " harvest for " + harvest_date + " written to mongo ")
+        except Exception as err:
+            current_app.logger.info("Error: unable to connect to mongodb, {}", err)
+        return
+
+    def load_repositories(self):
+        repositories = {}
+        try:
+            mongo_url = os.environ.get('MONGO_URL')
+            mongo_dbname = os.environ.get('MONGO_DBNAME')
+            repository_collection_name = os.environ.get('REPOSITORY_COLLECTION', 'jstor_repositories')
+            mongo_url = os.environ.get('MONGO_URL')
+            mongo_dbname = os.environ.get('MONGO_DBNAME')
+            mongo_client = MongoClient(mongo_url, maxPoolSize=1)
+
+            mongo_db = mongo_client[mongo_dbname]
+            repository_collection = mongo_db[repository_collection_name]
+            repos = repository_collection.find({})
+            for r in repos:
+                k = r["_id"]
+                v = r["displayname"]
+                repositories[k] = v 
+            mongo_client.close()
+            return repositories
+        except Exception as err:
+            current_app.logger.info("Error: unable to load repository table from mongodb, {}", err)
+            return repositories
 
     def revert_task(self, job_ticket_id, task_name):
         return True
