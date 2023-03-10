072458f3672035ea2613bc9b60a20308a26077d9
Chip Goines <chip_goines@harvard.edu>
2023-03-10 18:21:30 -0500

    rm temp files

--- ltstools/bin/concat-files.sh
+++ ltstools/bin/concat-files.sh
@@ -9,7 +9,9 @@ TODAYSDATE=`date -d'today' +"%Y%m%d"`
 #LIBRARYCLOUD
 find /tmp/JSTORFORUM/transformed -type f |  grep -v 'hollis' | grep -v 'aspace'| xargs cat > /tmp/JSTORFORUM/export/lc/via_export_incr_$TODAYSDATE.tmp
 cat /home/jstorforumadm/ltstools/conf/viacollhead.txt /tmp/JSTORFORUM/export/lc/via_export_incr_$TODAYSDATE.tmp /home/jstorforumadm/ltstools/conf/viacolltail.txt > /tmp/JSTORFORUM/export/lc/via_export_incr_$TODAYSDATE.xml
+rm -f /tmp/JSTORFORUM/export/lc/via_export_incr_$TODAYSDATE.tmp
 
 #PRIMO
 find /tmp/JSTORFORUM/transformed -type f | grep 'hollis' | xargs cat > /tmp/JSTORFORUM/via_export_incr_$TODAYSDATE.tmp
 cat /home/jstorforumadm/ltstools/conf/viacollhead.txt /tmp/JSTORFORUM/export/primo/via_export_incr_$TODAYSDATE.tmp /home/jstorforumadm/ltstools/conf/viacolltail.txt > /tmp/JSTORFORUM/export/primo/via_export_incr_$TODAYSDATE.xml
+rm -f /tmp/JSTORFORUM/via_export_incr_$TODAYSDATE.tmp
