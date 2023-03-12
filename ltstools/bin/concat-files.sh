0fcbd8b55680599f5da62499c2586e1ecb2b3252
Chip Goines <chip_goines@harvard.edu>
2023-03-11 19:27:27 -0500

    tamino header/footer for primo concatenated xml file

--- ltstools/bin/concat-files.sh
+++ ltstools/bin/concat-files.sh
@@ -13,5 +13,5 @@ rm -f /tmp/JSTORFORUM/export/lc/via_export_incr_$TODAYSDATE.tmp
 
 #PRIMO
 find /tmp/JSTORFORUM/transformed -type f | grep 'hollis' | xargs cat > /tmp/JSTORFORUM/export/primo/via_export_incr_$TODAYSDATE.tmp
-cat /home/jstorforumadm/ltstools/conf/viacollhead.txt /tmp/JSTORFORUM/export/primo/via_export_incr_$TODAYSDATE.tmp /home/jstorforumadm/ltstools/conf/viacolltail.txt > /tmp/JSTORFORUM/export/primo/via_export_incr_$TODAYSDATE.xml
+cat /home/jstorforumadm/ltstools/conf/taminohead.txt /tmp/JSTORFORUM/export/primo/via_export_incr_$TODAYSDATE.tmp /home/jstorforumadm/ltstools/conf/taminotail.txt > /tmp/JSTORFORUM/export/primo/via_export_incr_$TODAYSDATE.xml
 rm -f /tmp/JSTORFORUM/export/primo/via_export_incr_$TODAYSDATE.tmp
