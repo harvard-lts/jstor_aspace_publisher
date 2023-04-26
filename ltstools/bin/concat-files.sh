44ed493a4467e06c00751746699ebfc89f37886c
Chip Goines <chip_goines@harvard.edu>
2023-04-26 17:10:54 -0400

    SS-364 filename for full harvests for sets

--- ltstools/bin/concat-files.sh
+++ ltstools/bin/concat-files.sh
@@ -7,7 +7,7 @@
 TODAYSDATE=`date -d'today' +"%Y%m%d"`
 TARBALL=viaIncr_$TODAYSDATE.tar.gz
 
-while getopts ":s:d:u:f:" o; do
+while getopts ":s:d:u:f:l:" o; do
   case "${o}" in
      s)
        s=${OPTARG}
@@ -21,6 +21,9 @@ while getopts ":s:d:u:f:" o; do
      f)
        f=${OPTARG}
        ;;
+     l)
+       l=${OPTARG}
+       ;;
   esac
 done
 shift $((OPTIND-1))
@@ -29,6 +32,7 @@ SETNAME=${s}
 HARVESTDATE=${d}
 UNTILDATE=${u}
 FROMDATE=${f}
+FULLRUN=${l}
 
 if [ -n $HARVESTDATE ] && [ -n $UNTILDATE ]
 then
@@ -45,28 +49,63 @@ then
 fi
 
 
-if [-n $SETNAME ]
+if [ -n $SETNAME ]
 then
-  TARBALL=viaIncr_$SETNAME_$DATESTAMP.tar.gz
-  find /tmp/JSTORFORUM/transformed/$SETNAME -type f |  grep -v 'hollis' | grep -v 'aspace'| xargs cat > /tmp/JSTORFORUM/export/lc/via_export_incr_$SETNAME_$DATESTAMP.tmp
-  cat /home/jstorforumadm/ltstools/conf/viacollhead.txt /tmp/JSTORFORUM/export/lc/via_export_incr_$SETNAME_$DATESTAMP.tmp /home/jstorforumadm/ltstools/conf/viacolltail.txt > /tmp/JSTORFORUM/export/lc/via_export_incr_$SETNAME_$DATESTAMP.xml
-  tar -czvf /tmp/JSTORFORUM/export/lc/$TARBALL /tmp/JSTORFORUM/export/lc/via_export_incr_$SETNAME_$DATESTAMP.xml
-  rm -f /tmp/JSTORFORUM/export/lc/via_export_incr_$SETNAME_$DATESTAMP.tmp
+  if [ -n $FULLRUN ]
+  then
+    #LIBRARYCLOUD
+    TARBALL=viafull_$SETNAME_$DATESTAMP.tar.gz
+    find /tmp/JSTORFORUM/transformed/$SETNAME -type f |  grep -v 'hollis' | grep -v 'aspace'| xargs cat > /tmp/JSTORFORUM/export/lc/viafull_$SETNAME_$DATESTAMP.tmp
+    cat /home/jstorforumadm/ltstools/conf/viacollhead.txt /tmp/JSTORFORUM/export/lc/viafull_$SETNAME_$DATESTAMP.tmp /home/jstorforumadm/ltstools/conf/viacolltail.txt > /tmp/JSTORFORUM/export/lc/viafull_$SETNAME_$DATESTAMP.xml
+    tar -czvf /tmp/JSTORFORUM/export/lc/$TARBALL /tmp/JSTORFORUM/export/lc/viafull_$SETNAME_$DATESTAMP.xml
+    rm -f /tmp/JSTORFORUM/export/lc/viafull_$SETNAME_$DATESTAMP.tmp
 
-  find /tmp/JSTORFORUM/transformed/$SETNAME -type f | grep 'hollis' | xargs cat > /tmp/JSTORFORUM/export/primo/via_export_incr_$SETNAME_$DATESTAMP.tmp
-  cat /home/jstorforumadm/ltstools/conf/taminohead.txt /tmp/JSTORFORUM/export/primo/via_export_incr_$SETNAME_$DATESTAMP.tmp /home/jstorforumadm/ltstools/conf/taminotail.txt > /tmp/JSTORFORUM/export/primo/via_export_incr_$SETNAME_$DATESTAMP.xml
-  tar -czvf /tmp/JSTORFORUM/export/primo/$TARBALL /tmp/JSTORFORUM/export/primo/via_export_incr_$SETNAME_$DATESTAMP.xml
-  rm -f /tmp/JSTORFORUM/export/primo/via_export_incr_$SETNAME_DATESTAMP.tmp
+    #PRIMO
+    find /tmp/JSTORFORUM/transformed/$SETNAME -type f | grep 'hollis' | xargs cat > /tmp/JSTORFORUM/export/primo/viafull_$SETNAME_$DATESTAMP.tmp
+    cat /home/jstorforumadm/ltstools/conf/taminohead.txt /tmp/JSTORFORUM/export/primo/viafull_$SETNAME_$DATESTAMP.tmp /home/jstorforumadm/ltstools/conf/taminotail.txt > /tmp/JSTORFORUM/export/primo/viafull_$SETNAME_$DATESTAMP.xml
+    tar -czvf /tmp/JSTORFORUM/export/primo/$TARBALL /tmp/JSTORFORUM/export/primo/viafull_$SETNAME_$DATESTAMP.xml
+    rm -f /tmp/JSTORFORUM/export/primo/viafull_$SETNAME_$DATESTAMP.tmp
+  else
+    #LIBRARYCLOUD
+    TARBALL=viaIncr_$SETNAME_$DATESTAMP.tar.gz
+    find /tmp/JSTORFORUM/transformed/$SETNAME -type f |  grep -v 'hollis' | grep -v 'aspace'| xargs cat > /tmp/JSTORFORUM/export/lc/via_export_incr_$SETNAME_$DATESTAMP.tmp
+    cat /home/jstorforumadm/ltstools/conf/viacollhead.txt /tmp/JSTORFORUM/export/lc/via_export_incr_$SETNAME_$DATESTAMP.tmp /home/jstorforumadm/ltstools/conf/viacolltail.txt > /tmp/JSTORFORUM/export/lc/via_export_incr_$SETNAME_$DATESTAMP.xml
+    tar -czvf /tmp/JSTORFORUM/export/lc/$TARBALL /tmp/JSTORFORUM/export/lc/via_export_incr_$SETNAME_$DATESTAMP.xml
+    rm -f /tmp/JSTORFORUM/export/lc/via_export_incr_$SETNAME_$DATESTAMP.tmp
+
+    #PRIMO
+    find /tmp/JSTORFORUM/transformed/$SETNAME -type f | grep 'hollis' | xargs cat > /tmp/JSTORFORUM/export/primo/via_export_incr_$SETNAME_$DATESTAMP.tmp
+    cat /home/jstorforumadm/ltstools/conf/taminohead.txt /tmp/JSTORFORUM/export/primo/via_export_incr_$SETNAME_$DATESTAMP.tmp /home/jstorforumadm/ltstools/conf/taminotail.txt > /tmp/JSTORFORUM/export/primo/via_export_incr_$SETNAME_$DATESTAMP.xml
+    tar -czvf /tmp/JSTORFORUM/export/primo/$TARBALL /tmp/JSTORFORUM/export/primo/via_export_incr_$SETNAME_$DATESTAMP.xml
+    rm -f /tmp/JSTORFORUM/export/primo/via_export_incr_$SETNAME_$DATESTAMP.tmp
+  fi
 else
-  #LIBRARYCLOUD
-  find /tmp/JSTORFORUM/transformed -type f |  grep -v 'hollis' | grep -v 'aspace'| xargs cat > /tmp/JSTORFORUM/export/lc/via_export_incr_$TODAYSDATE.tmp
-  cat /home/jstorforumadm/ltstools/conf/viacollhead.txt /tmp/JSTORFORUM/export/lc/via_export_incr_$TODAYSDATE.tmp /home/jstorforumadm/ltstools/conf/viacolltail.txt > /tmp/JSTORFORUM/export/lc/via_export_incr_$TODAYSDATE.xml
-  tar -czvf /tmp/JSTORFORUM/export/lc/$TARBALL /tmp/JSTORFORUM/export/lc/via_export_incr_$TODAYSDATE.xml
-  rm -f /tmp/JSTORFORUM/export/lc/via_export_incr_$TODAYSDATE.tmp
+  if [ -n $FULLRUN ]
+  then
+    TARBALL=viafull_$TODAYSDATE.tar.gz
+    #LIBRARYCLOUD
+    find /tmp/JSTORFORUM/transformed -type f |  grep -v 'hollis' | grep -v 'aspace'| xargs cat > /tmp/JSTORFORUM/export/lc/viafull_$TODAYSDATE.tmp
+    cat /home/jstorforumadm/ltstools/conf/viacollhead.txt /tmp/JSTORFORUM/export/lc/viafull_$TODAYSDATE.tmp /home/jstorforumadm/ltstools/conf/viacolltail.txt > /tmp/JSTORFORUM/export/lc/viafull_$TODAYSDATE.xml
+    tar -czvf /tmp/JSTORFORUM/export/lc/$TARBALL /tmp/JSTORFORUM/export/lc/viafull_$TODAYSDATE.xml
+    rm -f /tmp/JSTORFORUM/export/lc/viafull_$TODAYSDATE.tmp
 
-  #PRIMO
-  find /tmp/JSTORFORUM/transformed -type f | grep 'hollis' | xargs cat > /tmp/JSTORFORUM/export/primo/via_export_incr_$TODAYSDATE.tmp
-  cat /home/jstorforumadm/ltstools/conf/taminohead.txt /tmp/JSTORFORUM/export/primo/via_export_incr_$TODAYSDATE.tmp /home/jstorforumadm/ltstools/conf/taminotail.txt > /tmp/JSTORFORUM/export/primo/via_export_incr_$TODAYSDATE.xml
-  tar -czvf /tmp/JSTORFORUM/export/primo/$TARBALL /tmp/JSTORFORUM/export/primo/via_export_incr_$TODAYSDATE.xml
-  rm -f /tmp/JSTORFORUM/export/primo/via_export_incr_$TODAYSDATE.tmp
+    #PRIMO
+    find /tmp/JSTORFORUM/transformed -type f | grep 'hollis' | xargs cat > /tmp/JSTORFORUM/export/primo/viafull_$TODAYSDATE.tmp
+    cat /home/jstorforumadm/ltstools/conf/taminohead.txt /tmp/JSTORFORUM/export/primo/viafull_$TODAYSDATE.tmp /home/jstorforumadm/ltstools/conf/taminotail.txt > /tmp/JSTORFORUM/export/primo/viafull_$TODAYSDATE.xml
+    tar -czvf /tmp/JSTORFORUM/export/primo/$TARBALL /tmp/JSTORFORUM/export/primo/viafull_$TODAYSDATE.xml
+    rm -f /tmp/JSTORFORUM/export/primo/viafull_$TODAYSDATE.tmp
+  else
+    TARBALL=viaIncr_$TODAYSDATE.tar.gz
+    #LIBRARYCLOUD
+    find /tmp/JSTORFORUM/transformed -type f |  grep -v 'hollis' | grep -v 'aspace'| xargs cat > /tmp/JSTORFORUM/export/lc/via_export_incr_$TODAYSDATE.tmp
+    cat /home/jstorforumadm/ltstools/conf/viacollhead.txt /tmp/JSTORFORUM/export/lc/via_export_incr_$TODAYSDATE.tmp /home/jstorforumadm/ltstools/conf/viacolltail.txt > /tmp/JSTORFORUM/export/lc/via_export_incr_$TODAYSDATE.xml
+    tar -czvf /tmp/JSTORFORUM/export/lc/$TARBALL /tmp/JSTORFORUM/export/lc/via_export_incr_$TODAYSDATE.xml
+    rm -f /tmp/JSTORFORUM/export/lc/via_export_incr_$TODAYSDATE.tmp
+
+    #PRIMO
+    find /tmp/JSTORFORUM/transformed -type f | grep 'hollis' | xargs cat > /tmp/JSTORFORUM/export/primo/via_export_incr_$TODAYSDATE.tmp
+    cat /home/jstorforumadm/ltstools/conf/taminohead.txt /tmp/JSTORFORUM/export/primo/via_export_incr_$TODAYSDATE.tmp /home/jstorforumadm/ltstools/conf/taminotail.txt > /tmp/JSTORFORUM/export/primo/via_export_incr_$TODAYSDATE.xml
+    tar -czvf /tmp/JSTORFORUM/export/primo/$TARBALL /tmp/JSTORFORUM/export/primo/via_export_incr_$TODAYSDATE.xml
+    rm -f /tmp/JSTORFORUM/export/primo/via_export_incr_$TODAYSDATE.tmp
+  fi
 fi
